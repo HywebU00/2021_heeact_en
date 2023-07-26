b60994aab6126a301296e650f6bcf5a395fbc5d5
mingshuya <mingshuya@gmail.com>
2023-07-26 15:52:08 +0800

    20230726 上傳

---
+++ js/customize.js
@@ -0,0 +1,60 @@
+// 自行加入的JS請寫在這裡
+
+$(function() {
+	 //燈箱slick+lightBox組合
+    $('.cp_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        focusOnSelect: true,
+        accessibility: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 545,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false
+            }
+        }]
+    });
+    $('.cp_slider').slickLightbox({
+        caption: 'caption',
+        useHistoryApi: 'true',
+        lazy: true
+    });
+
+
+//mpSlider
+$('.mpSlider').slick({
+    dots: true,
+    arrow: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    fade: true,
+    cssEase: 'ease'
+});
+
+});
